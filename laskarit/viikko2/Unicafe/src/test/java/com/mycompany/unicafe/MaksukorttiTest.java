727d1094ec7adc1b06eb4cd36ec2e850f0c31d55
Jamb000h <8905786+Jamb000h@users.noreply.github.com>
2019-11-08 10:58:24 +0200

    Testit luokalle Maksukortti

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void alkusaldoOikein() {
+        assertEquals(10, kortti.saldo());   
+    }
+    
+    @Test
+    public void saldolatausToimiiOikein() {
+        kortti.lataaRahaa(5);
+        assertEquals(15, kortti.saldo());   
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(5);
+        assertEquals(5, kortti.saldo());   
+    }
+    
+    @Test
+    public void saldoEiMuutuJosOtetaanRahaaYliSaldon() {
+        kortti.otaRahaa(15);
+        assertEquals(10, kortti.saldo());   
+    }
+    
+    @Test
+    public void rahanOttaminenPalauttaaTrueJosSaldoRiittaa() {
+        assertTrue(kortti.otaRahaa(10));   
+    }
+    
+    @Test
+    public void rahanOttaminenPalauttaaFalseJosSaldoEiRiita() {
+        assertFalse(kortti.otaRahaa(15));   
+    }
 }
