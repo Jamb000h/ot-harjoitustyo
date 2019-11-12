c5f1644755be59176d1ce658f9c61d1b565208a9
Jamb000h <8905786+Jamb000h@users.noreply.github.com>
2019-11-12 13:34:57 +0200

    Lisää JavaFX

---
+++ kuukausibudjetti/src/main/java/kuukausibudjetti/Main.java
@@ -0,0 +1,13 @@
+package kuukausibudjetti;
+
+import kuukausibudjetti.ui.BudjettiUI;
+
+/**
+ *
+ * @author Jonne
+ */
+public class Main {
+    public static void main(String[] args) {
+        BudjettiUI.main(args);
+    }
+}
